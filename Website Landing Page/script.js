da960a0383880988cbad25673ad7b2c14260a020
Riko Londong <39473496+rikolondong@users.noreply.github.com>
2026-06-26 09:30:39 +0700

    Add files via upload

---
+++ Website Landing Page/script.js
@@ -0,0 +1,11 @@
+// Mengubah tampilan navbar saat di-scroll
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.style.padding = '10px 0';
+        header.style.background = 'rgba(255, 255, 255, 0.95)';
+    } else {
+        header.style.padding = '0';
+        header.style.background = '#ffffff';
+    }
+});
